3321c52168aa7dbe90dc98d1ca2e372976d19238
Urfha <mifhaqz@gmail.com>
2026-06-28 22:57:20 +0800

    bugfix: payment validation check payment url

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&family=Playfair+Display:ital,wght@0,700;1,400&display=swap" rel="stylesheet">
-    <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="SB-Mid-client-3_Df4e8IB3Ci6BbJ"></script>
+    @php $snapUrl = config('services.midtrans.is_production') ? 'https://app.midtrans.com/snap/snap.js' : 'https://app.sandbox.midtrans.com/snap/snap.js'; @endphp
+    <script type="text/javascript" src="{{ $snapUrl }}" data-client-key="{{ config('services.midtrans.client_key') }}"></script>
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
 <body class="m-0 p-0 bg-[#09090b]">
